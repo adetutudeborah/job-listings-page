1dbfb0f4bd9ded0c4fe46d7a190dfca1037df93b
Adetutu Oluwasanmi <oluwasanmiadetutu@gmail.com>
2023-03-15 00:22:27 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css'
 import Header from './components/Header/Header';
-import Jobs from './components/Jobs/jobs';
+import Jobs from './components/Jobs/Jobs';
 
 function App() {
   return (
