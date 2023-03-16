042dbaebc70ce744eb10514cfaa8af2b0a292261
Adetutu Oluwasanmi <oluwasanmiadetutu@gmail.com>
2023-03-16 21:21:38 +0100

    updated Jobs.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,23 @@ import React from 'react';
 import './App.css'
 import Header from './components/Header/Header';
 import Jobs from './components/Jobs/Jobs';
+import data from "./data"
 
 function App() {
+
+  const JobSection = data.map(item => {
+    return (
+      <Jobs 
+         key={item.id}
+         item={item}
+      />
+    )
+  })
+
   return (
     <div className="App">
           <Header />
-          <Jobs />
+          {JobSection}
      </div>
   )
 }
--- src/components/Jobs/Jobs.jsx
+++ src/components/Jobs/Jobs.jsx
@@ -2,12 +2,42 @@ import React from "react";
 import "./Jobs.css"; 
 
 
-function Jobs () {
+
+function Jobs (props) {
     return (
         <div className="jobs">
-             <h1 className="jobheading">Jobs</h1>
+
+            <div className="aboutrole">
+
+                <div className="companylogo">
+                   <img src={props.item.logo} alt="company-logo" className="companylogo" />
+                </div>
+
+                <div className="roleinfo">
+                    <h1 className="companyname">{props.item.company}</h1>
+                    <h2 className="role">{props.item.position}</h2>
+
+                    <p className="rolesubinfo">
+                        <span className="date">{props.item.postedAt}</span>
+                        <img src="https://img.icons8.com/material-rounded/24/null/full-stop.png" className="period"/>
+                        <span className="contract">{props.item.contract}</span>
+                        <img src="https://img.icons8.com/material-rounded/24/null/full-stop.png " className="period"/>
+                        <span className="country">{props.item.location}</span>
+                    </p>
+                </div>
+
+            </div>
+           
+            <div className="rolekeywords">
+                <p className="keyword">{props.item.role}</p>
+                <p className="keyword">{props.item.level}</p>
+                <p className="keyword">{props.item.languages[0]}</p>
+                <p className="keyword">{props.item.languages[1]}</p>
+                <p className="keyword">{props.item.languages[2]}</p>
+                <p className="keyword">{props.item.tools[0]}</p>
+                <p className="keyword">{props.item.tools[1]}</p>
+            </div>
         </div>
-       
     )
 }
 
